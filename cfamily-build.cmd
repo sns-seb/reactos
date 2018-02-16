0303dd5c371f8f21288402226887b2ff9816c5a2
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2018-02-16 12:47:26 +0100

    use cfamily-build.cmd of commit peachee-cfamily@4ff7d46
    cfamily-build.cmd which succesfully built reactOs before February 15th

--- cfamily-build.cmd
+++ cfamily-build.cmd
@@ -1,21 +1,7 @@
 
-curl -O http://www.7-zip.org/a/7z1801-x64.exe
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-7z1801-x64.exe /S /D=%CD%\7zip
-dir
-if %errorlevel% neq 0 exit /b %errorlevel%
-dir 7zip
-set PATH=%CD%\7zip;%PATH%
-7z
-
-curl -O https://svn.reactos.org/amine/RosCMakeNinja.zip
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-7z x RosCMakeNinja.zip -oRosCMakeNinja
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-set PATH=%CD%\RosCMakeNinja\bin;%PATH%
+curl -L -O https://cmake.org/files/v3.9/cmake-3.9.1-win32-x86.zip
+unzip -o cmake-3.9.1-win32-x86.zip
+set PATH=%PATH%;%CD%\cmake-3.9.1-win32-x86\bin
 
 git apply --whitespace=fix cfamily-build.patch
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -23,17 +9,19 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 REM x86 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-cmake --version
-ninja --version
 
-rmdir /Q /S rosbuild
-mkdir rosbuild
-cd rosbuild
+rmdir /Q /S output-VS-i386
+rmdir /Q /S output-VS-amd64
+rmdir /Q /S output-VS-i386-sln
+rmdir /Q /S output-VS-amd64-sln
 
-call ..\configure.cmd -DENABLE_ROSTESTS=1
+call configure.cmd VSSolution
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-..\build-wrapper-win-x86\build-wrapper-win-x86-64.exe --out-dir ..\cfamily-compilation-database ninja
-if %errorlevel% neq 0 exit /b %errorlevel%
+cd output-VS-amd64-sln
+
+REM disable msbuild node reuse
+set MSBUILDDISABLENODEREUSE=1
+..\build-wrapper-win-x86\build-wrapper-win-x86-64.exe --out-dir ..\cfamily-compilation-database %MSBUILD15_PATH% /t:rebuild reactos.sln
 
 dir ..\cfamily-compilation-database
